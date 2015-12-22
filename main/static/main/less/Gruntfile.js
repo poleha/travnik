c13315dca9ab6576bd41d40e8d15906d36ba7a6a
Alex Poleha <alex@poleha.ru>
2015-12-22 16:43:34 +0300

    changes

---
+++ main/static/main/less/Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "../css/styles.css": "styles.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        //files: ['less/**/*.less'], // which files to watch
+        files: ['*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
